daa0ade97849c93b0ec8dd2d1c012e969dfbf65b
Emmanuel Hadoux <emmanuel.hadoux@gmail.com>
2019-10-04 14:43:16 +0100

    Conditional Quasar

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,20 @@
 import Vue from 'vue';
+import { Quasar } from 'quasar';
 
 import '../styles/quasar.styl';
 import iconSet from 'quasar/icon-set/fontawesome-v5';
 import lang from 'quasar/lang/en-gb';
 import '@quasar/extras/roboto-font/roboto-font.css';
 import '@quasar/extras/fontawesome-v5/fontawesome-v5.css';
-import { Quasar } from 'quasar';
+
 import PChatbox from './Chatbox.vue';
 
-Vue.use(Quasar, {
-  lang,
-  iconSet,
-});
-
-export default PChatbox;
+export default (withQuasar = true) => {
+  if (withQuasar) {
+    Vue.use(Quasar, {
+      lang,
+      iconSet,
+    });
+  }
+  return PChatbox;
+};
